a3885d3c71e18ba396e843beb3d5a09c6e86b1f8
Rebi.boy <rebjok911@gmail.com>
2022-10-22 15:51:21 +0100

    Final commit

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from operator import is_
+import math
+
+
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    track = 1
+    number = 3
+
+    while track < number_of_primes:
+
+        is_prime = True
+        for i in range(3, int(math.sqrt(number) + 1), 2):
+            if number % i == 0:
+                is_prime = False
+
+        if is_prime:
+            list.append(number)
+            track += 1
+
+        number += 2
     return list
